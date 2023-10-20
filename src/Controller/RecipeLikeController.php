0e61ff599ae800d039b8e1dfb669e28467e93d44
Dorine Muco <muco.dorine@gmail.com>
2023-10-20 13:16:32 +0200

    avoid babel

--- src/Controller/RecipeLikeController.php
+++ src/Controller/RecipeLikeController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Repository\RecipeRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RecipeLikeController extends AbstractController{
     #[Route('/recipe/like', name: 'recipe_like')]
-    public function recipeLike(UserRepository $rep,  Request $req){
+    public function recipeLike(UserRepository $rep,RecipeRepository $rep2,  Request $req){
         $id=$req->get('id');
-        dd($id);
+        // dd($id);
+        $recipe=$rep2->find($id);
+        $user=$rep->$this->user;
+        $user->addRecipe($recipe);
         return $this -> render('recipe_like/index.html.twig');
         
     }
