f8c5f1426abd332c545ef3bde0bf2509047e1d23
Dorine Muco <muco.dorine@gmail.com>
2023-10-20 12:23:59 +0200

    suite ajout favoris

--- src/Controller/RecipeLikeController.php
+++ src/Controller/RecipeLikeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LivreLikeController extends AbstractController{
+class RecipeLikeController extends AbstractController{
     #[Route('/recipe/like', name: 'recipe_like')]
-    public function recipeLike(){
+    public function recipeLike(UserRepository $rep,  Request $req){
+        $id=$req->get('id');
+        dd($id);
         return $this -> render('recipe_like/index.html.twig');
         
     }
