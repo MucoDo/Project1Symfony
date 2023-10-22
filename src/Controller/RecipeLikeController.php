a41be803901133cecfdb7cae14d2580445922d50
Dorine Muco <muco.dorine@gmail.com>
2023-10-22 08:54:36 +0200

    ajout liste des favoris

--- src/Controller/RecipeLikeController.php
+++ src/Controller/RecipeLikeController.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use App\Repository\RecipeRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -24,14 +25,25 @@ class RecipeLikeController extends AbstractController{
         $em = $doctrine->getManager();
         $em->flush();
 
-        $favoris= $user->getRecipes()->toArray();
+        // Renvoyer une response vide sinon symfony va générer une erreur disant que la réponse n'a pas été envoyée
+        return new Response();
+        // $favoris= $user->getRecipes()->toArray();
 
-        dd($favoris);
-        $vars=['favoris' => $favoris];
+        // // dd($favoris);
+        // $vars=['favoris' => $favoris];
 
-        return $this -> render('recipe_like/index.html.twig',$vars);
+        // return $this -> render('recipe_like/index.html.twig',$vars);
         
     }
+    #[Route('/recipe/show/likes', name: 'recipe_show_likes')]
+    public function recipeShowLikes(UserRepository $rep, ManagerRegistry $doctrine){
+        $user = $this->getUser();
+        $favoris= $user->getRecipes()->toArray();
 
+        // dd($favoris);
+        $vars=['favoris' => $favoris];
+
+        return $this -> render('recipe_like/recipe_show_likes.html.twig',$vars);
+    }
 
 }
