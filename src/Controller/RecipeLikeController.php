6a74eee4a858b44936912cf69dacd8e8e4c4839c
Dorine Muco <muco.dorine@gmail.com>
2023-11-15 21:45:31 +0100

    ---

--- src/Controller/RecipeLikeController.php
+++ src/Controller/RecipeLikeController.php
@@ -9,51 +9,69 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RecipeLikeController extends AbstractController{
     #[Route('/recipe/like', name: 'recipe_like')]
-    public function recipeLike(ManagerRegistry $doctrine,UserRepository $rep,RecipeRepository $rep2,  Request $req){
+    public function recipeLike(ManagerRegistry $doctrine,UserRepository $user,RecipeRepository $rep2,  Request $req){
         $id=$req->get('id');
         // dd($id);
         $recipe=$rep2->find($id);
         // dd($recipe);
         $user = $this->getUser();
-        // dd($user);
-        
-        $user->addRecipe($recipe);
-
+        $containsRecipe =  $user->getRecipes()->contains($recipe);
+        // dd($containsRecipe);
+        $heartColor='bleu';
+        if ($containsRecipe){
+            $user->removeRecipe($recipe);
+            $heartColor='bleu';
+            
+        }
+        else{
+            $user->addRecipe($recipe); 
+            $heartColor='red';
+        }
+      
         $em = $doctrine->getManager();
+
         $em->flush();
 
+
+        $response= ['heartColor'=>$heartColor];
+
+       
+    
+
         // Renvoyer une response vide sinon symfony va générer une erreur disant que la réponse n'a pas été envoyée
-        return new Response();
+        // return new Response();
+        return new JsonResponse($response);
 
         
     }
 
 
 
-    #[Route('/recipe/unlike', name: 'recipe_unlike')]
-    public function recipeUnlike(ManagerRegistry $doctrine,UserRepository $rep,RecipeRepository $rep2,  Request $req){
-        $id=$req->get('id');
-        // dd($id);
-        $recipe=$rep2->find($id);
-        // dd($recipe);
-        $user = $this->getUser();
-        // dd($user);
+    // #[Route('/recipe/unlike', name: 'recipe_unlike')]
+    // public function recipeUnlike(ManagerRegistry $doctrine,UserRepository $rep,RecipeRepository $rep2,  Request $req){
+    //     $id=$req->get('id');
+    //     // dd($id);
+    //     $recipe=$rep2->find($id);
+    //     // dd($recipe);
+    //     $user = $this->getUser();
+    //     // dd($user);
         
-        $user->removeRecipe($recipe);
+    //     $user->removeRecipe($recipe);
 
-        $em = $doctrine->getManager();
-        $em->flush();
+    //     $em = $doctrine->getManager();
+    //     $em->flush();
 
-        // Renvoyer une response vide sinon symfony va générer une erreur disant que la réponse n'a pas été envoyée
-        return new Response();
-        // $favoris= $user->getRecipes()->toArray();
+    //     // Renvoyer une response vide sinon symfony va générer une erreur disant que la réponse n'a pas été envoyée
+    //     return new Response();
+    //     // $favoris= $user->getRecipes()->toArray();
 
 
         
-    }
+    // }
 
     #[Route('/recipe/show/likes', name: 'recipe_show_likes')]
     public function recipeShowLikes(UserRepository $rep, ManagerRegistry $doctrine){
