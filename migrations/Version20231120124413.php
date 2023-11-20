05d3914127488ae040e1113740681da5eddc8598
Dorine Muco <muco.dorine@gmail.com>
2023-11-20 14:14:28 +0100

    ---

--- migrations/Version20231120124413.php
+++ migrations/Version20231120124413.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231112210518 extends AbstractMigration
+final class Version20231120124413 extends AbstractMigration
 {
     public function getDescription(): string
     {
